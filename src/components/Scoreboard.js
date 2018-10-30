e24ccdc52c8f687c0e05323ff69fec83c405e349
Hoan Phung <hoanphung.ptso1@gmail.com>
2018-10-30 15:27:55 +0100

    scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,50 +1,86 @@
 import React, { Component } from 'react'
 import Player from './Player'
+import AddPlayer from './AddPlayer'
 import './Scoreboard.css'
 
-const players = [
-    {
-        id: 1,
-        name: 'Real Madrid',
-        score: 4
-    },
-    {
-        id: 2,
-        name: 'Barcelona',
-        score: 3
-    },
-    {
-        id: 3,
-        name: 'Ronaldo',
-        score: 8
-    },
-    {
-        id: 4,
-        name: 'Messi',
-        score: 10
-    }
-]
-
 export default class Scoreboard extends Component {
-    renderPlayer(player) {
-        return <Player 
-            key={player.id}
-            name={player.name}
-            score={player.score}
-        />
+    state = {
+        players: [
+            {
+                id: 1,
+                name: 'Real Madrid',
+                score: 4
+            },
+            {
+                id: 2,
+                name: 'Barcelona',
+                score: 3
+            },
+            {
+                id: 3,
+                name: 'Ronaldo',
+                score: 8
+            },
+            {
+                id: 4,
+                name: 'Messi',
+                score: 10
+            }
+        ]
     }
-    
+
+    updatePlayerScore = (id, score) => {
+        const updatedPlayers = this.state.players.map(
+            player => {
+                if (player.id === id) {
+                    return {
+                        ...player,
+                        score
+                    }
+                }
+                else {
+                    return player
+                }
+            }
+        )
+        this.setState({ players: updatedPlayers })
+    }
+
+    addPlayer = (name) => {
+        const player = {
+          id: Math.round(Math.random()*100000),
+          name,
+          score: 0
+        }
+        this.setState({
+          players: this.state.players.concat(player)
+        })
+    }
+
+    renderPlayer = (player) => {
+        return (
+            <Player
+                id={player.id}
+                name={player.name}
+                score={player.score}
+                key={player.id}
+                updatePlayerScore={this.updatePlayerScore}
+            />
+        )
+    }
+
     render() {
         return (
             <div className='scoreboard'>
                 <h1>Scoreboard</h1>
                 <ul>
                     {
-                        players
+                        this.state.players
                             .sort((a, b) => b.score - a.score)
                             .map(this.renderPlayer)
-                    }
+                    }                    
                 </ul>
+                <h3><AddPlayer addPlayer={this.addPlayer} /></h3>
             </div>
         )
     }
